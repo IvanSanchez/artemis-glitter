6eb0278aa57683311563fd354d11427c127e2fc7
Matthew Long <matthew.long@aptomar.com>
2014-06-23 22:15:52 +0200

    Removing some extra comments.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,14 @@ module.exports = function(grunt) {
     nodewebkit: {
       options: {
         build_dir: './build', // Where the build version of my node-webkit app is saved
-        mac: true, // We want to build it for mac
-        win: true, // We want to build it for win
-        linux32: true, // We don't need linux32
-        linux64: true, // We don't need linux64
-        app_version: '0.1',
+        // Build all platforms we can
+        mac: true,
+        win: true,
+        linux32: true,
+        linux64: true,
+
+        // This will be set from the package.json, but add if you want a different version
+        // app_version: '0.1.0',
         app_name: 'Glitter for Artemis'
         // TODO: you'll want to have a credits.html somewhere then enable the following:
         // credits: 'credits.html',
